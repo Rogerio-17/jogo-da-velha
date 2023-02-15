9215b89a9f6cdc030583bfd6444facf62aedf8ef
Rogerio-17 <rogeriojmf10@gmail.com>
2023-02-15 08:39:58 -0300

    estilizando o jogo e comecando a adicionar o js

---
+++ main.js
@@ -0,0 +1,11 @@
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let bottons = document.querySelectorAll("#buttons-container button");
+let message = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
+let secondPlayer;
+
+// contador de jogadas
+let player1 = 0;
+let player2 = 0;
